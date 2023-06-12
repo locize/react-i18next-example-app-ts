1dddbf00e806b5589abec45adba0c4399e8faa62
Adriano Raiano <adriano@raiano.ch>
2023-06-12 11:41:17 +0200

    resources-for-ts

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 This is a TypeScript version of [this](https://github.com/locize/react-i18next-example-app).
 
 The `src/@types/i18next.d.ts` file also enables typesafe translations.
-During development, you can run `npm run downloadLocales`. And on runtime the translations are directly downloaded from the locize CDN.
+During development, you can run `npm run resources-for-ts`. And on runtime the translations are directly downloaded from the locize CDN.
--- src/@types/i18next.d.ts
+++ src/@types/i18next.d.ts
@@ -1,12 +1,7 @@
-import enTranslation from './locales/en/translation.json';
+import resources from './resources.json';
 
 declare module 'i18next' {
   interface CustomTypeOptions {
-    resources: {
-      translation: typeof enTranslation;
-    };
-    // if you see an error like: "Argument of type 'DefaultTFuncReturn' is not assignable to parameter of type xyz"
-    // set returnNull to false (and also in the i18next init options)
-    // returnNull: false;
+    resources: typeof resources;
   }
 }
